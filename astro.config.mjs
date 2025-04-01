7251473d8c0f5aabbb80e28979a2b7ea43606195
DavidUmiri <davidumiri@gmail.com>
2025-04-01 12:53:00 +0200

    build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   site: 'https://davidumiri.github.io',
-  base: '/astro-ecommerce'
+  base: '/astro-ecommerce',
+  output: 'static',
+  build: {
+    assets: 'assets'
+  }
 });
